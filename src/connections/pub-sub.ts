7e9c120ff7ed14b26a5f0738b81671ac0c0058ff
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-10-22 21:03:42 +0100

    migrated to ioredis 5

--- src/connections/pub-sub.ts
+++ src/connections/pub-sub.ts
@@ -1,16 +1,11 @@
 import Redis from "ioredis";
 import { getLogger } from "../utils/logger";
+import { createRedisClient } from "./redis";
 
 const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_DB } = process.env;
 
-var pubClient: Redis = new Redis(REDIS_PORT, REDIS_HOST, {
-	password: REDIS_PASSWORD,
-	db: REDIS_DB,
-});
-const subClient = Redis(REDIS_PORT, REDIS_HOST, {
-	password: REDIS_PASSWORD,
-	db: REDIS_DB,
-});
+var pubClient: Redis = createRedisClient();
+const subClient: Redis = createRedisClient();
 Promise.all([pubClient.connect(), subClient.connect()]);
 
 export async function getPubSubClients() {
--- src/connections/redis.ts
+++ src/connections/redis.ts
@@ -3,11 +3,13 @@ import { getLogger } from "../utils/logger";
 
 const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_DB } = process.env;
 
-var redisClient = new Redis(REDIS_PORT, REDIS_HOST, {
-	password: REDIS_PASSWORD,
-	db: REDIS_DB,
-});
-
+export function createRedisClient() {
+	return new Redis(parseInt(REDIS_PORT), REDIS_HOST, {
+		password: REDIS_PASSWORD,
+		db: parseInt(REDIS_DB),
+	});
+}
+var redisClient = createRedisClient();
 redisClient.on("connect", async () => {
 	const logger = await getLogger();
 	logger.success("Connected to Redis");
